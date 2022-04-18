ee84ec113b4842b99db9d9ad239660f162ea2622
narmstro2020 <narmstro@warren.k12.in.us>
2022-04-18 12:00:27 -0400

    Final Form

--- src/org/WalkerCC_CompSci/Game.java
+++ src/org/WalkerCC_CompSci/Game.java
@@ -64,12 +64,64 @@ public class Game {
     }
 
     private boolean CheckForWin() {
-        //Implement this method.
+        Space zero = this.gameBoard.spaces[0];
+        Space one = this.gameBoard.spaces[1];
+        Space two = this.gameBoard.spaces[2];
+        Space three = this.gameBoard.spaces[3];
+        Space four = this.gameBoard.spaces[4];
+        Space five = this.gameBoard.spaces[5];
+        Space six = this.gameBoard.spaces[6];
+        Space seven = this.gameBoard.spaces[7];
+        Space eight = this.gameBoard.spaces[8];
+
+        //zero, one, two
+        if(zero == one && one == two && zero != Space.EMPTY){
+            return true;
+        }
+
+        //three, four, five
+        if(three == four && four == five && three != Space.EMPTY){
+            return true;
+        }
+
+        //six, seven, eight
+        if(six == seven && seven == eight && six != Space.EMPTY){
+            return true;
+        }
+
+        //zero, three, six
+        if(zero == three && three == six && zero != Space.EMPTY){
+            return true;
+        }
+
+        //one, four, seven
+        if(one == four && four == seven && one != Space.EMPTY){
+            return true;
+        }
+
+        //two, five, eight
+        if(two == five && five == eight && two != Space.EMPTY){
+            return true;
+        }
+
+        //zero, four, eight
+        if(zero == four && four == eight && zero != Space.EMPTY){
+            return true;
+        }
+
+        //two, four, six
+        if(two == four && four == six && two != Space.EMPTY){
+            return true;
+        }
         return false;
     }
 
     private boolean IsFilled(){
-        //Implement this method.
+        for(int i = 0; i < this.gameBoard.spaces.length; i++){
+            if(this.gameBoard.spaces[i] == Space.EMPTY){
+                return true;
+            }
+        }
         return false;
     }
 
